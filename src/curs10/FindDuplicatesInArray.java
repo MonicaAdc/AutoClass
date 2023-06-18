0c40c9cd82f6132997dd360ad7e08d7afe279377
unknown <monicaadochitei@gmail.com>
2023-06-18 11:35:13 +0300

    curs 10 final

--- src/curs10/FindDuplicatesInArray.java
+++ src/curs10/FindDuplicatesInArray.java
@@ -1,5 +1,7 @@
 package curs10;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class FindDuplicatesInArray {
@@ -13,18 +15,45 @@ public class FindDuplicatesInArray {
 		//index					//	0		1		2		3	 4		5
 		//lenght=6
 		
-		for(int i=0; i< myStringArray.length; i++) {
-			for(int j=i+1; j< myStringArray.length; j++) {
+		for(int i = 0; i < myStringArray.length; i++) {
+			
+			for(int j= i+1; j< myStringArray.length; j++) {
+				
 				if(myStringArray[i].equals(myStringArray[j])) {
-					System.out.println("Nume duplicat: "+ myStringArray[i]);
+					
+					System.out.println("Nume duplicat: " + myStringArray[i]);
 				}
 			}
-		System.out.println("------------------------");	
+			
+		}
 		
+		System.out.println("--------------------------------");
 		
+		Set<String> duplicat = gasesteDuplicat(myStringArray);
+		for(String nume : duplicat) {
+			System.out.println("Nume duplicat: " + nume);
+		}
+		
+	}
+	
+	public static Set<String> gasesteDuplicat(String[] myStringArray){
+		Set<String> numeDuplicat =  new HashSet <>();
+		
+		for(int i = 0; i < myStringArray.length; i++) {
+			
+			for(int j= i+1; j< myStringArray.length; j++) {
+				
+				if(myStringArray[i].equals(myStringArray[j])) {
+					
+					numeDuplicat.add(myStringArray[i]);
+					//System.out.println("Nume duplicat: " + myStringArray[i]);
+				}
+			}
+			
+		}
+		
+		return numeDuplicat;
+	}
 	
 
-	}
-	}
 }
-
--- src/homework6/SwitchEx.java
+++ src/homework6/SwitchEx.java
@@ -15,37 +15,35 @@ public class SwitchEx {
 		
 		int bonus;
 	 
-	switch(an) {
-		case 1:
-				System.out.println("Bonusul este 100");
-			break;
-		case 2:
-				System.out.println("Bonusul este 200");
-			break;
-		case 3:
-			if (vanzari <=500)
-				System.out.println("Bonusul este 600");
-		
-			else {	
-			if (vanzari<= 100000 && luna<=6 )
-				System.out.println("Bonusul este 800");
-		
-			if(vanzari<=100000 && luna<=7 && luna<=11 )
-				System.out.println("Bonusul este 1000");
-			
-			
-			if(vanzari<=100000 && luna==12 )
-				System.out.println(" In decembrie se vand singure!Bonusul este 900");
-			 
-
-			if (vanzari > 100000)
-				System.out.println("Bonusul este 1200");
-			}
-			break;
-			
-			default:
-				if(an>3);
-				System.out.println("Nu se ofera niciun bonus");
+		switch(an) {
+			case 1:
+					System.out.println("Bonusul este 100");
+				break;
+			case 2:
+					System.out.println("Bonusul este 200");
+				break;
+			case 3:
+				if (vanzari <=500) {
+					System.out.println("Bonusul este 600");
+				}
+				else if (vanzari<= 10000)
+				{
+					if (luna <= 6)
+					{System.out.println("Bonusul este 800");
+					}
+					else if (luna <= 11)
+					{System.out.println("Bonusul este 1000");
+					}
+					else if (luna == 12)
+					{System.out.println(" In decembrie se vand singure!Bonusul este 900");
+					}
+				}
+				else if (vanzari > 10000) {
+					System.out.println("Bonusul este 1200");
+				}
+				break;
+		default:
+		System.out.println("Nu se ofera niciun bonus");
 					
 	}
 
